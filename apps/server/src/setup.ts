22fa0e4c4a4e7b65eb76f13f340d93ced2a2ac49
Mohammad <mohammad@admins-MacBook-Pro.local>
2025-04-01 11:39:29 +0330

    fixing infinite loading problem

--- apps/server/src/setup.ts
+++ apps/server/src/setup.ts
@@ -2,19 +2,10 @@ import { cors } from '@elysiajs/cors'
 import { Elysia } from 'elysia'
 import { Database } from 'bun:sqlite'
 
-const allowedOriginRegex = /http:\/\/localhost:\d+/
-
 export const setup = new Elysia({ name: 'setup' })
   .use(
     cors({
-      origin: ({ headers }) => {
-        const origin = headers.get('origin')
-        return (
-          !origin ||
-          origin === 'chrome-extension://nnjokgfpoecefilcbmcinacgmefmdabl' ||
-          allowedOriginRegex.test(origin)
-        )
-      },
+      origin: true,
       credentials: true,
       allowedHeaders: [
         'Content-Type',
