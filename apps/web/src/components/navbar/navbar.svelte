ea4d35daa7ec7929a90cc3fc5b5e0473ba043fcd
Mohammad <mohammad@admins-MacBook-Pro.local>
2025-04-01 23:52:43 +0330

    added navbar to the webmode

---
+++ apps/web/src/components/navbar/navbar.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+</script>
+
+<div class=".bg-[#bd2e4f] .text-white .px-10 .py-[5px] .h-[50px] .flex .items-center .fixed .top-0 .left-0 .right-0 .z-50">
+  <img 
+    src="https://cdn.prod.website-files.com/67053ca02c9605b542296d0f/671389353d1aca320568d70b_logo-light.png"
+    alt="Uprent"
+    class=".w-[103px] .h-auto"
+  />
+</div> 
--- apps/web/src/lib/pages/feed/ui/feed.svelte
+++ apps/web/src/lib/pages/feed/ui/feed.svelte
@@ -1,13 +1,17 @@
 <script lang="ts">
-  import { FeedProperties } from '.'
   import { feed } from '$lib/shared/stores'
   import { onMount } from 'svelte'
+  import { Navbar } from '../../../../components/navbar'
+  import FeedProperties from './feed-properties.svelte'
 
   onMount(() => {
     feed.reload()
   })
 </script>
 
-<div class=".flex .max-h-screen .flex-col .bg-gray-50">
-  <FeedProperties />
+<div class=".flex .flex-col .h-full">
+  <Navbar />
+  <div class=".flex .max-h-screen .flex-col .bg-gray-50 .pt-[50px]">
+    <FeedProperties />
+  </div>
 </div>
